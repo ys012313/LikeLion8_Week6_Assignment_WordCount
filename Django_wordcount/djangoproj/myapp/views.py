599bae80dd4d37223ed5b9584e6867804d381b37
ridohyun@likelion.org <ri.dohyun@gmail.com>
2020-05-22 17:10:04 +0900

    update wordcount

---
+++ Django_wordcount/djangoproj/myapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'wordcount/home.html') 
+
+def about(request):
+    return render(request, 'wordcount/about.html') 
